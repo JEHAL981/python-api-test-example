79f56a25628fc5f87b3492a1e5bf6b369b7b9232
James Halls <jameshalls@Jamess-MacBook-Pro.local>
2020-03-27 09:17:39 +0000

    Update method name

--- test/test_helloworld.py
+++ test/test_helloworld.py
@@ -1,7 +1,7 @@
 from HelloWolrd import app
 
 
-def test_hello_world():
+def test_hello_world_should_return_200_status():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
